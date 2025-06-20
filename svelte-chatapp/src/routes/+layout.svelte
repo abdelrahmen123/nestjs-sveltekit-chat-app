38438df7e3e04b0290ed2fbefd56fe1a94312f9a
Abdelrahmen <Abdelrahmen@gmail.com>
2025-06-20 16:15:23 +0300

    init app and preper db and orm

---
+++ svelte-chatapp/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>Chat App</title>
+</svelte:head>
+
+<main class="flex min-h-screen flex-col">
+
+	{@render children()}
+
+</main>
